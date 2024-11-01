5fb20a2db3e46e173a71508c28f801801f084784
Egor Pankratiev <egor.pankratiew@gmail.com>
2024-11-01 23:07:55 +0300

    added google tests

---
+++ tests/AESEncryptorTest.cpp
@@ -0,0 +1,16 @@
+﻿#include "aes/AESDecryptor.hpp"
+#include "aes/AESEncryptor.hpp"
+#include "gtest/gtest.h"
+
+TEST(AESEncryptorTest, EncryptionDecryptionTest) {
+    const std::vector<byte> key(CryptoPP::AES::MAX_KEYLENGTH, 42);
+    const std::vector<byte> data{ 'T', 'e', 's', 't', ' ', 'd', 'a', 't', 'a' };
+    const AESEncryptor aes_encryptor(key);
+    const AESDecryptor aes_decryptor(key);
+
+    const std::vector<byte> ciphertext = aes_encryptor.encrypt(data);
+    const std::vector<byte> decrypted = aes_decryptor.decrypt(ciphertext);
+    
+    EXPECT_EQ(data.size(), decrypted.size());
+    EXPECT_EQ(data, decrypted);
+}
---
+++ tests/DHClientServerTest.cpp
@@ -0,0 +1,27 @@
+﻿#include "dh/DHClient.hpp"
+#include "dh/DHServer.hpp"
+#include "utils/WinSockUtils.hpp"
+#include "gtest/gtest.h"
+#include <integer.h>
+
+TEST(DHClientServerTest, KeyExchangeTest) {
+    WinSockUtils::initializeWinSock();
+    
+    constexpr u_short port = 3030;
+    DHServer server;
+    DHClient client;
+    CryptoPP::Integer serverSharedSecret, clientSharedSecret;
+
+    std::thread serverThread([&] {
+        ASSERT_TRUE(server.startListening(port));
+        serverSharedSecret = server.exchangeKeys();
+    });
+
+    ASSERT_TRUE(client.connectToServer("127.0.0.1", port));
+    clientSharedSecret = client.exchangeKeys();
+
+    serverThread.join();
+
+    EXPECT_EQ(serverSharedSecret, clientSharedSecret);
+    WinSockUtils::cleanupWinSock();
+}
