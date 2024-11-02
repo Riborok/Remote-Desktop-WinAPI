bedec97b613f2f34a6d9fc7e148de1ffdb0ebdb9
Egor Pankratiev <egor.pankratiew@gmail.com>
2024-11-02 10:12:50 +0300

    added jpg for tests

--- tests/AESEncryptorTest.cpp
+++ tests/AESEncryptorTest.cpp
@@ -3,8 +3,21 @@
 #include "aes/AESEncryptor.hpp"
 #include "gtest/gtest.h"
 
-TEST(AESEncryptorTest, EncryptionDecryptionTest) {
-    const std::vector<byte> data(readFileToBuffer("test.txt"));
+TEST(AESEncryptorTest, EncryptionDecryptionTestTXT) {
+    const std::vector<byte> data(readFileToBuffer("res/test.txt"));
+    const std::vector<byte> key(CryptoPP::AES::MAX_KEYLENGTH, 42);
+    const AESEncryptor aes_encryptor(key);
+    const AESDecryptor aes_decryptor(key);
+
+    const std::vector<byte> ciphertext = aes_encryptor.encrypt(data);
+    const std::vector<byte> decrypted = aes_decryptor.decrypt(ciphertext);
+    
+    EXPECT_EQ(data.size(), decrypted.size());
+    EXPECT_EQ(data, decrypted);
+}
+
+TEST(AESEncryptorTest, EncryptionDecryptionTestJPG) {
+    const std::vector<byte> data(readFileToBuffer("res/test.jpg"));
     const std::vector<byte> key(CryptoPP::AES::MAX_KEYLENGTH, 42);
     const AESEncryptor aes_encryptor(key);
     const AESDecryptor aes_decryptor(key);
--- tests/CompressionUtilsTest.cpp
+++ tests/CompressionUtilsTest.cpp
@@ -2,8 +2,18 @@
 #include "utils/CompressionUtils.hpp"
 #include "gtest/gtest.h"
 
-TEST(CompressionUtilsTest, CompressAndDecompress) {
-    const std::vector<byte> data(readFileToBuffer("test.txt"));
+TEST(CompressionUtilsTest, CompressAndDecompressTXT) {
+    const std::vector<byte> data(readFileToBuffer("res/test.txt"));
+    const std::vector<byte> compressedData = CompressionUtils::compress(data);
+    const std::vector<byte> decompressedData = CompressionUtils::decompress(compressedData);
+    
+    EXPECT_GT(data.size(), compressedData.size());
+    EXPECT_EQ(data.size(), decompressedData.size());
+    EXPECT_EQ(data, decompressedData);
+}
+
+TEST(CompressionUtilsTest, CompressAndDecompressJPG) {
+    const std::vector<byte> data(readFileToBuffer("res/test.jpg"));
     const std::vector<byte> compressedData = CompressionUtils::compress(data);
     const std::vector<byte> decompressedData = CompressionUtils::decompress(compressedData);
     
