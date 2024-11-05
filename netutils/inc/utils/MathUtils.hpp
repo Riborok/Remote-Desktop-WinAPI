6bf1bd11c2eeffd48c2cb26d081667b8a6a98e67
Egor Pankratiev <egor.pankratiew@gmail.com>
2024-11-05 15:53:53 +0300

    changed MathUtils::ceil to template function with type constraints

--- netutils/inc/utils/MathUtils.hpp
+++ netutils/inc/utils/MathUtils.hpp
@@ -1,8 +1,14 @@
 ﻿#pragma once
 
+#include <type_traits>
+
 class MathUtils {
 public:
     MathUtils() = delete;
-    
-    static size_t ceil(const size_t numerator, const size_t denominator);
+
+    template <typename T, typename U>
+    static auto ceil(T numerator, U denominator) ->
+            std::enable_if_t<std::is_integral_v<T> && std::is_integral_v<U>, std::common_type_t<T, U>> {
+        return (numerator + denominator - 1) / denominator;
+    }
 };
