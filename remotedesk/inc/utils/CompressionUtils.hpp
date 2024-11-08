57db647ecb521c5032bebabc11dc5ecb203ecd4c
Egor Pankratiev <egor.pankratiew@gmail.com>
2024-11-08 19:46:24 +0300

    changed class structure for CompressionUtils

--- remotedesk/inc/utils/CompressionUtils.hpp
+++ remotedesk/inc/utils/CompressionUtils.hpp
@@ -10,7 +10,10 @@ public:
     CompressionUtils() = delete;
     
     static std::vector<byte> compress(const std::vector<byte>& data);
+    static std::vector<byte> compress(const byte* data, const size_t dataSize);
+    
     static std::vector<byte> decompress(const std::vector<byte>& compressedData);
+    static std::vector<byte> decompress(const byte* compressedData, const size_t compressedDataSize);
 private:
-    static std::vector<byte> transform(CryptoPP::BufferedTransformation& bt, const std::vector<byte>& data);
+    static std::vector<byte> transform(CryptoPP::BufferedTransformation& bt, const byte* data, const size_t dataSize);
 };
--- remotedesk/src/utils/CompressionUtils.cpp
+++ remotedesk/src/utils/CompressionUtils.cpp
@@ -1,18 +1,26 @@
 ﻿#include "../../inc/utils/CompressionUtils.hpp"
 
 std::vector<byte> CompressionUtils::compress(const std::vector<byte>& data) {
+    return compress(data.data(), data.size());
+}
+
+std::vector<byte> CompressionUtils::compress(const byte* data, const size_t dataSize) {
     CryptoPP::ZlibCompressor compressor;
-    return transform(compressor, data);
+    return transform(compressor, data, dataSize);
 }
 
 std::vector<byte> CompressionUtils::decompress(const std::vector<byte>& compressedData) {
-    CryptoPP::ZlibDecompressor decompressor;
-    return transform(decompressor, compressedData);
+    return decompress(compressedData.data(), compressedData.size());
 }
 
-std::vector<byte> CompressionUtils::transform(CryptoPP::BufferedTransformation& bt, const std::vector<byte>& data) {
+std::vector<byte> CompressionUtils::decompress(const byte* compressedData, const size_t compressedDataSize) {
+    CryptoPP::ZlibDecompressor decompressor;
+    return transform(decompressor, compressedData, compressedDataSize);
+}
+
+std::vector<byte> CompressionUtils::transform(CryptoPP::BufferedTransformation& bt, const byte* data, const size_t dataSize) {
     std::vector<byte> result;
-    bt.Put(data.data(), data.size());
+    bt.Put(data, dataSize);
     bt.MessageEnd();
     result.resize(bt.MaxRetrievable());
     bt.Get(result.data(), result.size());
