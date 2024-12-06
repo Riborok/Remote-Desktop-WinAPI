36dda13d39ee9981edced85b700257088a115315
Egor Pankratiev <egor.pankratiew@gmail.com>
2024-12-06 23:09:22 +0300

    added more limits in TypeLimits

--- remotedesk/inc/utils/TypeLimits.hpp
+++ remotedesk/inc/utils/TypeLimits.hpp
@@ -1,8 +1,7 @@
 ﻿#pragma once
 
 #include <limits>
-
-#include "../types/byte.hpp"
+#include <windows.h>
 
 class TypeLimits {
 public:
@@ -10,7 +9,11 @@ public:
     
     static constexpr size_t MAX_SIZE_T_VALUE = (std::numeric_limits<size_t>::max)();
     static constexpr byte MAX_BYTE_VALUE = (std::numeric_limits<byte>::max)();
-
+    static constexpr DWORD MAX_DWORD_VALUE = (std::numeric_limits<DWORD>::max)();
+    static constexpr WORD MAX_WORD_VALUE = (std::numeric_limits<WORD>::max)();
+    
     static constexpr size_t MIN_SIZE_T_VALUE = (std::numeric_limits<size_t>::min)();
     static constexpr byte MIN_BYTE_VALUE = (std::numeric_limits<byte>::min)();
+    static constexpr DWORD MIN_DWORD_VALUE = (std::numeric_limits<DWORD>::min)();
+    static constexpr WORD MIN_WORD_VALUE = (std::numeric_limits<WORD>::min)();
 };
